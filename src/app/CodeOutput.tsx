216786b85da2740b9d799d0a846529b498d56ecf
Joël J. Présent <36854931+joeljpresent@users.noreply.github.com>
2024-08-17 13:43:41 +0200

    Rename runner.ts

--- src/app/CodeOutput.tsx
+++ src/app/CodeOutput.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { FormEvent } from "react";
-import { CodeError } from "./parser";
+import { CodeError } from "./runner";
 
 export default function CodeOutput({
   output, error, isTextMode, onToggleTextMode
