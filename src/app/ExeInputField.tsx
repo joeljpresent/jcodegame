b88e92973d6a436e4989deee5c9cf9cf0f554c49
Joël J. Présent <36854931+joeljpresent@users.noreply.github.com>
2024-08-23 12:23:14 +0200

    Allow char literals in Exe Input Field

--- src/app/ExeInputField.tsx
+++ src/app/ExeInputField.tsx
@@ -2,8 +2,7 @@
 
 import { FormEvent, useState } from "react";
 import TextModeToggle from "./TextModeToggle";
-import { createInputError, createInputSuccess, ExeInput } from "./exe/input";
-import { parseInt32 } from "./utils/int32";
+import { ExeInput, parseInputField } from "./exe/input";
 import ExeInputDisplay from "./ExeInputDisplay";
 
 export default function ExeInputField({ input, onChange }: Props) {
@@ -12,33 +11,15 @@ export default function ExeInputField({ input, onChange }: Props) {
 
   function handleToggleTextMode() {
     setIsTextMode(!isTextMode);
-    const newInput = parse(text, !isTextMode);
+    const newInput = parseInputField(text, !isTextMode);
     onChange(newInput);
   }
 
   function handleTextChange(e: FormEvent<HTMLTextAreaElement>) {
     const newText = e.currentTarget.value;
     setText(newText);
-    const newInput = parse(newText, isTextMode);
+    const newInput = parseInputField(newText, isTextMode);
     onChange(newInput);
-  }
-
-  function parse(text: string, isTextMode: boolean): ExeInput {
-    try {
-      if (isTextMode) {
-        const codePoints = Array.from(text).map(c => c.codePointAt(0)!);
-        return createInputSuccess(codePoints);
-      }
-      const numbers = text
-        .trim()
-        .split(/\s*,\s*/g)
-        .filter(s => s !== "")
-        .map(s => parseInt32(s));
-      return createInputSuccess(numbers);
-    }
-    catch (err: unknown) {
-      return createInputError(String(err));
-    }
   }
 
   return <>
--- src/app/exe/input.ts
+++ src/app/exe/input.ts
@@ -1,3 +1,6 @@
+import { parseInt32 } from "../utils/int32";
+import { parseCharLiteral } from "../utils/unicode";
+
 interface ExeInputSuccess {
   status: "success",
   value: number[],
@@ -22,4 +25,26 @@ export function createInputError(errorMessage: string): ExeInput {
     status: "error",
     error: errorMessage,
   };
+}
+
+export function parseInputField(text: string, isTextMode: boolean): ExeInput {
+  try {
+    if (isTextMode) {
+      const codePoints = Array.from(text).map(c => c.codePointAt(0)!);
+      return createInputSuccess(codePoints);
+    }
+    const numbers = text
+      .trim()
+      .split(/\s*,\s*/g)
+      .filter(s => s !== "")
+      .map(s => (
+        s.charAt(0) === "'"
+          ? parseCharLiteral(s)
+          : parseInt32(s)
+      ));
+    return createInputSuccess(numbers);
+  }
+  catch (err: unknown) {
+    return createInputError(String(err));
+  }
 }
