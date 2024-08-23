a8004d1146994f95bcf5108ddf5d155b6aaaf9a6
Joël J. Présent <36854931+joeljpresent@users.noreply.github.com>
2024-08-23 10:28:55 +0200

    ExeInputVisualizer: show next input to be read

--- src/app/ExeInputVisualizer.tsx
+++ src/app/ExeInputVisualizer.tsx
@@ -2,22 +2,62 @@
 
 import { useState } from "react";
 import TextModeToggle from "./TextModeToggle";
-import { toUnicode } from "./utils";
+import { toUnicodeChar } from "./utils";
 
 export default function ExeInputVisualizer({ input, nextInputIdx }: Props) {
   const [isTextMode, setIsTextMode] = useState(false);
+
+  function getValueStyle(idx: number) {
+    if (idx === nextInputIdx) {
+      return {
+        fontWeight: "bold",
+        color: "cyan",
+      };
+    } else if (idx === nextInputIdx - 1) {
+      return {
+        color: "lime",
+      };
+    }
+  }
 
   function handleToggleTextMode() {
     setIsTextMode(!isTextMode);
   }
 
+  function endDisplay() {
+    return (
+      <span style={{
+        ...getValueStyle(input.length),
+        fontSize: "0.6em",
+      }}>
+        END
+      </span>
+    );
+  }
+
   function inputDisplay() {
-    if (input.length === 0) {
-      return <p><i>Empty input.</i></p>
-    } else if (isTextMode) {
-      return <p><pre>{toUnicode(input)}</pre></p>
+    if (isTextMode) {
+      return (
+        <p><pre>
+          {
+            input.map((v, idx) => (
+              <span style={getValueStyle(idx)}>
+                {toUnicodeChar(v)}
+              </span>
+            ))
+          }
+          {endDisplay()}
+        </pre></p>
+      );
     }
-    return <p>{input.join(", ")}</p>
+    return <p>
+      {
+        input.map((v, idx) => (
+          <span style={getValueStyle(idx)}>{v}, </span>
+        ))
+      }
+      {endDisplay()}
+    </p>;
   }
 
   return <>
